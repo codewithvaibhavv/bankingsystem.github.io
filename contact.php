6f7dac87832e0c7c8c12c33b330ef3e9f81593c5
VAIBHAV SHARMA <vaibhavsharma11061998@gmail.com>
2023-08-09 15:05:01 +0530

    Banking

---
+++ contact.php
@@ -0,0 +1,65 @@
+<?php
+    include "header.php";
+    include "navbar.php";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="contact_style.css">
+</head>
+
+<body>
+    <div class="flex-container-background">
+        <div class="flex-container-heading">
+            <h1 id="contact">Contact Us</h1>
+        </div>
+
+        <div class="flex-container" style="border-bottom: 0;
+                                           border-top-left-radius: 10px;
+                                           border-top-right-radius: 10px;">
+            <div class="flex-item">
+                <h1 id="sub-contact">Corporate Headquarters</h1>
+                <p id="sub-contact">
+                    Corporate HQ<br>NEW ERA BANK<br>
+                    Sec-6 E-80 <br>
+                    New Moradabad
+                </p>
+            </div>
+            <div class="flex-item">
+                <h1 id="sub-contact">General Contact</h1>
+                <p id="sub-contact">
+                    Toll-Free: 9760653801<br>
+                    Phone: +919760653801<br>
+                    <!--Fax: 732-367-2313<br>-->
+                    Email: vaibhav976065@gmail.com
+                </p>
+            </div>
+        </div>
+
+        <div class="flex-container" style="border-top: 0;
+                                           border-bottom-left-radius: 10px;
+                                           border-bottom-right-radius: 10px;">
+            <div class="flex-item">
+                <h1 id="sub-contact">Customer Care (24x7)</h1>
+                <p id="sub-contact">
+                    Toll-Free: 9760653801<br>
+                    Phone: +919760653801<br>
+                    Email: vaibhav976065@gmail.com
+                </p>
+            </div>
+            <div class="flex-item">
+                <h1 id="sub-contact">Live Chat</h1>
+                <p id="sub-contact">
+                    Download our app and live chat<br>
+                    with our customer care !<br>
+                    App available on Google Play<br>
+                    and iPhone-AppStore.
+                </p>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
